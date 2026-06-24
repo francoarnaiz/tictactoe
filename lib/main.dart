a20dec322d081785d19786e0b3eb1283cd03b1bd
francoarnaiz <franco.arnaiz@telaeris.com>
2026-06-24 14:03:53 -0700

    Grid Spot Refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -50,20 +50,26 @@ class GameGrid extends StatelessWidget {
             Row(
               spacing: 10,
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                for (var i = 0; i < 3; i++)
-                  Container(
-                    height: 100,
-                    width: 100,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.black),
-                    ),
-                    child: Center(child: Text("O")),
-                  ),
-              ],
+              children: [for (var i = 0; i < 3; i++) Spot("X")],
             ),
         ],
       ),
+    );
+  }
+}
+
+class Spot extends StatelessWidget {
+  const Spot(this.piece, {super.key});
+
+  final String piece;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 100,
+      decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+      child: Center(child: Text("X")),
     );
   }
 }
