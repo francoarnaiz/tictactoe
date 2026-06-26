f81ce4343b47f65b5a56479c15cdfa3fae7878a8
francoarnaiz <franco.arnaiz@telaeris.com>
2026-06-26 11:19:03 -0700

    Added win checking

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 const err = 0;
@@ -12,12 +13,6 @@ List<List<Piece>> board = [[], [], []];
 enum Piece { X, O, none }
 
 Piece nextPlayer = Piece.O;
-
-void fillBoard(Piece piece) {
-  board = [
-    for (var x = 0; x < 3; x++) [for (var x = 0; x < 3; x++) piece],
-  ];
-}
 
 String getPiece(Piece piece) {
   return switch (piece) {
@@ -81,10 +76,68 @@ class _GameGridState extends State<GameGrid> {
   var nextRow = 0;
   var nextColumn = 0;
   Piece nextPlayer = Piece.O;
+  var gameStatus = "";
 
   void submit() {
     placePiece(nextColumn, nextRow, nextPlayer);
+
+    var winner = checkWinner();
+    if (winner == Piece.O) {
+      gameStatus = "O wins!";
+    } else if (winner == Piece.X) {
+      gameStatus = "X wins!";
+    } else {
+      gameStatus = "Next player: ${getPiece(nextPlayer)}";
+    }
+
     updater.update();
+  }
+
+  void fillBoard(Piece piece) {
+    board = [
+      for (var x = 0; x < 3; x++) [for (var x = 0; x < 3; x++) piece],
+    ];
+  }
+
+  Piece checkWinner() {
+    var xWin = [Piece.X, Piece.X, Piece.X];
+    var oWin = [Piece.O, Piece.O, Piece.O];
+
+    for (var row in board) {
+      if (listEquals(row, xWin)) {
+        return Piece.X;
+      } else if (listEquals(row, oWin)) {
+        return Piece.O;
+      }
+    }
+
+    var boardColumns = [
+      for (var col in [0, 1, 2])
+        [
+          for (var row in [0, 1, 2]) board[row][col],
+        ],
+    ];
+
+    for (var column in boardColumns) {
+      if (listEquals(column, xWin)) {
+        return Piece.X;
+      } else if (listEquals(column, oWin)) {
+        return Piece.O;
+      }
+    }
+
+    for (var diagonal in [
+      [board[0][0], board[1][1], board[2][2]],
+      [board[0][2], board[1][1], board[2][0]],
+    ]) {
+      if (listEquals(diagonal, xWin)) {
+        return Piece.X;
+      } else if (listEquals(diagonal, oWin)) {
+        return Piece.O;
+      }
+    }
+
+    return Piece.none;
   }
 
   int addPiece(int column, int row, Piece piece) {
@@ -123,6 +176,7 @@ class _GameGridState extends State<GameGrid> {
   @override
   void initState() {
     super.initState();
+    gameStatus = "Next Player: O";
     fillBoard(Piece.none);
   }
 
@@ -133,7 +187,7 @@ class _GameGridState extends State<GameGrid> {
       builder: (context, _) {
         return Column(
           children: [
-            Text("Next Player: ${getPiece(nextPlayer)}"),
+            Text(gameStatus),
             SizedBox(
               height: 600,
               width: 600,
@@ -154,7 +208,6 @@ class _GameGridState extends State<GameGrid> {
                             (int column, int row) {
                               nextColumn = column;
                               nextRow = row;
-                              print("Selected row $nextRow, col $nextColumn");
                             },
                           ),
                       ],
